2249f625678221543d8333ea758c7329d3581ad0
William Reynolds <wrr3j@virginia.edu>
2013-05-16 17:27:33 -0700

    Add new metrics UI mockup to dashboard

--- media/js/remo/dashboard_mozillians.js
+++ media/js/remo/dashboard_mozillians.js
@@ -7,6 +7,29 @@ $(document).ready(function () {
         $('.dashboard-mozillians-reps-grid-button').parent().siblings().removeClass('active');
     });
 
+    // Activities time period click
+    $('#activities-month-button').click(function () {
+        $(this).parent().addClass('active');
+        $('#activities-month-button').parent().siblings().removeClass('active');
+    });
+    $('#activities-6months-button').click(function () {
+        $(this).parent().addClass('active');
+        $('#activities-6months-button').parent().siblings().removeClass('active');
+    });
+    $('#activities-future-button').click(function () {
+        $(this).parent().addClass('active');
+        $('#activities-future-button').parent().siblings().removeClass('active');
+    });
+    $('#activities-custom-button').click(function () {
+        $(this).parent().addClass('active');
+        $('#activities-custom-button').parent().siblings().removeClass('active');
+    });
+
+    // Advanced button click.
+    $('#adv-search-icon-dashboard').click(function() {
+        $('#adv-search').slideToggle();
+    });
+
     $('.dashboard-mozillians-reps-reports-button').click(function () {
         $('.dashboard-mozillians-reps-reports-block').removeClass('hidden');
         $('.dashboard-mozillians-reps-grid-block').addClass('hidden');
@@ -15,23 +38,19 @@ $(document).ready(function () {
         $('.dashboard-mozillians-reps-reports-button').parent().siblings().removeClass('active');
     });
 
-    $('.dashboard-events-future-button').click(function () {
-        $('.dashboard-events-future-block').removeClass('hidden');
-        $('.dashboard-events-past-block').addClass('hidden');
+    // Todo: Initialize datepickers for custom date range
 
-        $('.dashboard-events-future-button').parent().addClass('active');
-        $('.dashboard-events-future-button').parent().siblings().removeClass('active');
+    // Events expander button click.
+    $('.dashboard-events-expander-button').click(function () {
+        // TODO Toggle the icon between > and V when clicked
+        $('.dashboard-events-expander-button').toggle(function() {
+            $('.dashboard-events-expander-button').html('&#62;');
+        }, function () {
+            $('.dashboard-events-expander-button').html('/');
+        });
+
+        $('.dashboard-events-details-row').toggleClass('hidden');
     });
-
-    $('.dashboard-events-past-button').click(function () {
-        $('.dashboard-events-past-block').removeClass('hidden');
-        $('.dashboard-events-future-block').addClass('hidden');
-
-        $('.dashboard-events-past-button').parent().addClass('active');
-        $('.dashboard-events-past-button').parent().siblings().removeClass('active');
-    });
-
-
 
     $('table').each(function(index, item) { $(item).stupidtable(); });
     // Apply prettyDate on all elements with data-time attribute.
